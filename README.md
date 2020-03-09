f9c296ee9ee9c0eccec6edb36d1e1c037cfebe20
umakantasenapati <61902697+umakantasenapati@users.noreply.github.com>
2020-03-09 21:18:59 +0530

    Update memory management

--- README.md
+++ README.md
@@ -1,16 +1,11 @@
 
+
 MultiThreadedTCPserver: 
-Implementation of Multi-thread TCP server to handle multiple client request in  parallel.
-Current version of implementaion spwan seaparate thread  to handle each Client.
-Once all the clients send EOM massge  , Server shut down itself as no more service required.
+Implementation of Multi-thread TCP server to handle multiple client request in parallel. Current version of implementaion spwan seaparate thread to handle each Client. Once all the clients send EOM massge , Server shut down itself as no more service required.
 
 Build instructions:
-Ensure g++ compiler along with support for linux socket reletd lib  and c++11 standard thread library is present. Then run the below
-commands.
+Ensure g++ compiler along with support for linux socket reletd lib and c++11 standard thread library is present. Then run the below commands.
 
-to generate Server and Client executabe:
-make all
+to generate Server and Client executabe: make all
 
-to clean executable:
-make clean
-
+to clean executable: make clean
---
+++ myHostInfo.cpp
@@ -0,0 +1,77 @@
+#include "myHostInfo.h"
+#include "myLog.h"
+#include "myException.h"
+
+myHostInfo::myHostInfo()
+{
+	openHostDb();
+}
+
+myHostInfo::myHostInfo(string hostName,hostType type)
+{
+	
+	searchHostDB = 0;
+	
+	try 
+	{
+		if (type == NAME)
+		{
+			// Retrieve host by name
+			hostPtr = gethostbyname(hostName.c_str());
+
+			if (hostPtr == NULL)
+			{
+				
+					myException gethostbynameException(0,"error getting host by name");
+					throw gethostbynameException;
+				
+			}
+        }
+		else if (type == ADDRESS)
+		{
+			// Retrieve host by address
+		    unsigned long netAddr = inet_addr(hostName.c_str());
+			if (netAddr == -1)
+			{
+				myException inet_addrException(0,"Error calling inet_addr()");
+				throw inet_addrException;
+			}
+
+	        hostPtr = gethostbyaddr((char *)&netAddr, sizeof(netAddr), AF_INET);
+			if (hostPtr == NULL)
+			{
+				
+					myException gethostbynameException(0,"error getting host by name");
+					throw gethostbynameException;
+				
+			}
+        }
+		else
+		{
+			myException unknownTypeException(0,"unknown host type: host name/address has to be given ");
+			throw unknownTypeException;
+		}
+    }
+	catch(myException& excp)
+	{
+		excp.response();
+		exit(1);
+	}
+}
+
+
+
+char myHostInfo::getNextHost()
+{
+	// Get the next host from the database
+    if (searchHostDB == 1)
+    {
+         if ((hostPtr = gethostent()) == NULL)
+              return 0;
+         else
+              return 1;
+    }
+    return 0;
+}
+
+
